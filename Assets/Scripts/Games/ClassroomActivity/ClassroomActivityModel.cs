711d21699e4418d83d3c468351aeabbe87e57404
gutee <mgutierrez@tekgenesis.com>
2017-01-15 22:37:05 -0300

    Classroom activity.

---
+++ Assets/Scripts/Games/ClassroomActivity/ClassroomActivityModel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using SimpleJSON;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Scripts.Metrics.Model;
+
+namespace Assets.Scripts.Games.ClassroomActivity {
+	public class ClassroomActivityModel : LevelModel {
+		#region implemented abstract members of LevelModel
+		public override void NextChallenge() { }
+		public override void InitGame() { }
+		public override void RestartGame() { }
+		#endregion
+
+		private int currentLvl;
+		private List<ClassroomLevel> lvls;
+
+		public void NextLvl(){
+			currentLvl++;
+		}
+
+		public bool GameEnded(){
+			return currentLvl == lvls.Count;
+		}
+
+		public int CurrentLvl(){
+			return currentLvl;
+		}
+
+		public void SetCurrentLevel(ClassroomActivityView view, bool enabled) {
+			lvls[currentLvl].Set(view, enabled);
+		}
+
+		private ClassroomActivityModel(List<ClassroomLevel> lvls) {
+			this.lvls = lvls;
+			currentLvl = 0;
+			MetricsController.GetController().GameStart();
+
+		}
+
+		public static ClassroomActivityModel StartFromJson(JSONArray lvls){
+			List<ClassroomLevel> classLevels = new List<ClassroomLevel>();
+			foreach(JSONClass lvl in lvls) {
+				GameObject correct = OneGameObject(lvl["correct"]);
+				List<GameObject> wrong = GameObjects(lvl["wrong"].AsArray);
+
+				classLevels.Add(new ClassroomLevel(correct, wrong));
+			}
+
+			Debug.Log(classLevels.Count + " Levels");
+
+			return new ClassroomActivityModel(classLevels);
+		}
+
+		static List<GameObject> GameObjects(JSONArray lvl) {
+			return new List<JSONNode>(lvl.Childs).ConvertAll(OneGameObject);
+		}
+
+		static GameObject OneGameObject(JSONNode lvl) {
+			return GameObject.Find(lvl.Value);
+		}
+
+		public void Correct() {
+			LogAnswer(true);
+		}
+
+		public void Wrong(){
+			LogAnswer(false);
+		}
+	}
+}
---
+++ Assets/Scripts/Games/ClassroomActivity/ClassroomActivityView.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using SimpleJSON;
+using UnityEngine;
+using UnityEngine.UI;
+using Assets.Scripts.Sound;
+
+namespace Assets.Scripts.Games.ClassroomActivity {
+	public class ClassroomActivityView : LevelView {
+		#region implemented abstract members of LevelView
+		public override void RestartGame() { }
+		#endregion
+
+		public Image upperBoard;
+		public Button soundBtn;
+
+		private Sprite[] boards;
+		private ClassroomActivityModel model;
+
+		public void Start(){
+			model = ClassroomActivityModel.StartFromJson(JSON.Parse(Resources.Load<TextAsset>("Jsons/classroom").text).AsObject["levels"].AsArray);
+			boards = Resources.LoadAll<Sprite>("Sprites/ClassroomActivity/consignas");
+			Begin();
+		}
+
+		public void Begin(){
+			ShowExplanation();
+			Next(true);
+		}
+
+		public void SoundClick(){
+			SoundController.GetController().PlayClip(Resources.Load<AudioClip>("Audio/ClassroomActivity/Consignas/" + model.CurrentLvl()));
+		}
+
+		public void Next(bool first = false){
+			if(!first){
+				SetCurrentLevel(false);
+				model.NextLvl();
+			}
+
+			if(model.GameEnded()) EndGame();
+			else SetCurrentLevel(true);
+
+			SoundClick();
+		}
+
+		private void EndGame() {
+			Debug.Log("End game");
+		}
+
+		private void SetCurrentLevel(bool enabled) {
+			model.SetCurrentLevel(this, enabled);
+
+			upperBoard.sprite = boards[model.CurrentLvl()];
+		}
+
+		public void ClickCorrect(){
+			PlayRightSound();
+			model.Correct();
+			Next();
+		}
+
+		public void ClickWrong(){
+			PlayWrongSound();
+			model.Wrong();
+		}
+		
+	}
+}
