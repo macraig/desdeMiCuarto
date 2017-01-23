51c379abd1c02125b46f750f6ee0605c08307e95
gutee <mgutierrez@tekgenesis.com>
2017-01-22 22:54:27 -0300

    Neighbourhood activity started.

---
+++ Assets/Scripts/Games/NeighbourhoodActivity/NeighbourhoodActivityModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Assets.Scripts.Metrics.Model;
+
+namespace Assets.Scripts.Games.NeighbourhoodActivity {
+	public class NeighbourhoodActivityModel : LevelModel {
+		private int currentLvl;
+
+		public void NextLvl(){
+			currentLvl++;
+		}
+
+		public bool GameEnded(){
+			return currentLvl == 9;
+		}
+
+		public int CurrentLvl(){
+			return currentLvl;
+		}
+
+		public NeighbourhoodActivityModel() {
+			currentLvl = 0;
+			MetricsController.GetController().GameStart();
+
+		}
+
+		public void Correct() {
+			LogAnswer(true);
+		}
+
+		public void Wrong(){
+			LogAnswer(false);
+		}
+	}
+}
---
+++ Assets/Scripts/Games/NeighbourhoodActivity/NeighbourhoodActivityView.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Games.NeighbourhoodActivity {
+	public class NeighbourhoodActivityView : LevelView {
+		public Image upperBoard;
+		public Button soundBtn;
+
+		private NeighbourhoodActivityModel model;
+
+		public void Start(){
+			model = new NeighbourhoodActivityModel();
+			Begin();
+		}
+
+		public void Begin(){
+			ShowExplanation();
+
+		}
+
+		public void SoundClick(){
+			
+		}
+
+		override public void Next(bool first = false){
+			if(!first){
+				SetCurrentLevel(false);
+				model.NextLvl();
+			}
+
+			if(model.GameEnded()) EndGame(60,0,1250);
+			else SetCurrentLevel(true);
+
+			SoundClick();
+		}
+
+
+
+		private void SetCurrentLevel(bool enabled) {
+			
+		}
+
+		public void ClickCorrect(){
+			ShowRightAnswerAnimation ();
+			model.Correct();
+		}
+
+		public void ClickWrong(){
+			ShowWrongAnswerAnimation ();
+			model.Wrong();
+		}
+	}
+}
